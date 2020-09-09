71b6c921b249312a027c6e1697312698fc9a7c8b
Yue Su <34404957+yue-su@users.noreply.github.com>
2020-09-09 18:03:31 -0300

    added helpers

---
+++ schemes/scheme-model.js
@@ -0,0 +1,29 @@
+const db = require("../data/dbconfig")
+
+const find = () => {
+  return db("schemes")
+}
+const findById = () => {
+  db("schemes")
+}
+const add = () => {
+  db("schemes")
+}
+const remove = () => {
+  db("schemes")
+}
+const findSteps = () => {
+  db("schemes")
+}
+const update = () => {
+  db("schemes")
+}
+
+module.exports = {
+  find,
+  findById,
+  add,
+  remove,
+  findSteps,
+  update,
+}
